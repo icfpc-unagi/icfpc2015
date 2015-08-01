9e8bd9e83dc3d8a91bf9be58d7128462921819ab
imos <git@imoz.jp>
2015-08-01 18:24:16 +0900

    Make run.sh use imosh.

--- server/storage/run.sh
+++ server/storage/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+source "$(dirname "${BASH_SOURCE}")/../../imos-variables" || exit 1
+eval "${IMOSH_INIT}"
+
 exec {LOCK}>>"$(dirname "${BASH_SOURCE}")/lock"
 if ! flock -w 1 "${LOCK}"; then
   echo 'This script is already running, so exiting...' >&2
@@ -8,3 +11,4 @@ fi
 
 bash "$(dirname "${BASH_SOURCE}")/sync.sh" &
 ~/.dropbox-dist/dropboxd
+sub::exit
