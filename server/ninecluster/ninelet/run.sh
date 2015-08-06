d18dc566a66530c0b4937ef857e6e9147d47e3f2
imos <git@imoz.jp>
2015-08-06 17:55:34 +0900

    Install apache into ninecluster.

--- server/ninecluster/ninelet/run.sh
+++ server/ninecluster/ninelet/run.sh
@@ -7,17 +7,24 @@ start_docker() {
     docker run \
         --rm \
         --privileged \
-        --publish="${port}:22" \
+        --publish="${port}" \
         --volume=/alloc:/alloc \
         --volume=/mirror:/mirror:ro \
+        --volume=/mirror/dropbox/www:/var/www/html:ro \
         --volume=/ninemaster/tmp:/var/tmp \
         --volume=/ninemaster/config:/usr/imos/config \
-        local/ninelet /bin/bash -c \
-        "/usr/sbin/sshd -D \\
-            -o 'ClientAliveInterval 5' \\
-            -o 'ClientAliveCountMax 3' $*"
+        local/ninelet /bin/bash -c "$*"
     sleep 1
   done
+}
+
+start_ssh_docker() {
+  local port="${1}"; shift
+
+  start_docker "${port}:22" \
+      "/usr/sbin/sshd -D \\
+          -o 'ClientAliveInterval 5' \\
+          -o 'ClientAliveCountMax 3' $*"
 }
 
 cd "$(dirname $0)"
@@ -30,9 +37,9 @@ chmod ugo=rwxrwxrwt /ninemaster/tmp
 mkdir -p /ninemaster/config
 
 for port in $(seq 2201 2209); do
-  start_docker "${port}" &
+  start_ssh_docker "${port}" &
 done
 
 for port in $(seq 22000 22127); do
-  start_docker "${port}" -d &
+  start_ssh_docker "${port}" -d &
 done
