1feb5e1bb47c2da767f89865e00b964d45bb2814
sulume <sulume@gmail.com>
2015-08-08 06:17:32 +0900

    LCG test

--- src/sim/sim_test.cc
+++ src/sim/sim_test.cc
@@ -1,8 +1,18 @@
 #include "base/test.h"
 #include "src/sim/sim.h"
 
-class HelloWorldTest : public testing::Test {};
+class SimTest : public testing::Test {};
 
-TEST_F(HelloWorldTest, HelloWorld) {
-  EXPECT_TRUE(true);
+TEST_F(SimTest, LCGTest) {
+  LCG lcg(17);
+  EXPECT_EQ(lcg.next(), 0);
+  EXPECT_EQ(lcg.next(), 24107);
+  EXPECT_EQ(lcg.next(), 16552);
+  EXPECT_EQ(lcg.next(), 12125);
+  EXPECT_EQ(lcg.next(), 9427);
+  EXPECT_EQ(lcg.next(), 13152);
+  EXPECT_EQ(lcg.next(), 21440);
+  EXPECT_EQ(lcg.next(), 3383);
+  EXPECT_EQ(lcg.next(), 6873);
+  EXPECT_EQ(lcg.next(), 16117);
 }
