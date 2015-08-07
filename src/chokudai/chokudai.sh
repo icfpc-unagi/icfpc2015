e3d1b633e28bcdeee2c44dd0ceacb770e76a3acb
imos <git@imoz.jp>
2015-08-08 08:41:15 +0900

    Bug fix.

--- src/chokudai/chokudai.sh
+++ src/chokudai/chokudai.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 TARGET=''
-if [ -f "${BASH_SOURCE}.runfiles/ICFPC2015/ICFPC2015/Main.exe" ]; then
-  TARGET="${BASH_SOURCE}.runfiles/ICFPC2015/ICFPC2015/Main.exe"
+if [ -f "${BASH_SOURCE}.runfiles/src/chokudai/Main.exe" ]; then
+  TARGET="${BASH_SOURCE}.runfiles/src/chokudai/Main.exe"
 elif [ -f "$(dirname "${BASH_SOURCE}")/Main.exe" ]; then
   TARGET="$(dirname "${BASH_SOURCE}")/Main.exe"
 else
