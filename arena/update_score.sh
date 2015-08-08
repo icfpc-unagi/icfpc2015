96e7a9ab81b4fd4ab7856c12b77dfff444c841f0
imos <git@imoz.jp>
2015-08-08 12:06:35 +0900

    Bug fix: update_score.

--- arena/update_score.sh
+++ arena/update_score.sh
@@ -5,11 +5,16 @@ source imosh
 cd /alloc/dropbox/www/results
 
 for file in */*.json; do
-  if [ ! -f "../scores/${file}" ]; then
+  if [ ! -f "../scores/${file%.json}.txt" ]; then
     mkdir -p "$(dirname "../scores/${file}")"
-    /alloc/global/bin/sim_main \
+    if /alloc/global/bin/sim_main \
         --output_score \
         "/mirror/github/data/problems/${file##*/}" \
-        "${file}" > "../scores/${file%.json}.txt"
+        "${file}" > "../scores/${file%.json}.txt.bak"; then
+      mv "../scores/${file%.json}.txt.bak" "../scores/${file%.json}.txt"
+    else
+      rm "../scores/${file%.json}.txt.bak"
+      printf -1 > "../scores/${file%.json}.txt"
+    fi
   fi
 done
