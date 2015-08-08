1c3beff9c38fab9282e7166cef4d9733de8950cc
imos <git@imoz.jp>
2015-08-08 11:31:27 +0900

    Support jar.

---
+++ arena/update.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+source imosh
+
+AI=(/mirror/dropbox/arena/*)
+DATA=(/mirror/github/data/problems/*)
+
+run() {
+  local ai="${1}"
+  local data="${2}"
+
+  ai_name="${ai##*/}"
+  ai_path="/alloc/dropbox/www/results/${ai_name}"
+  mkdir -p "${ai_path}"
+  data_name="${data##*/}"
+  path="${ai_path}/${data_name}"
+
+  if [ ! -f "${path}" ]; then
+    echo "Generating ${path}..."
+    if [ "${ai_name%.exe}" != "${ai_name}" ]; then
+      if timeout 180s mono "${ai}" "${data}" > "${path}.bak"; then
+        mv "${path}.bak" "${path}"
+      else
+        rm "${path}.bak"
+        echo 'null' > "${path}"
+      fi
+    elif [ "${ai_name%.jar}" != "${ai_name}" ]; then
+      if timeout 180s \
+          java -cp "${ai}:/mirror/github/src/wata/gson-2.3.1.jar" \
+          Main "${data}" > "${path}.bak"; then
+        mv "${path}.bak" "${path}"
+      else
+        rm "${path}.bak"
+        echo 'null' > "${path}"
+      fi
+    else
+      true
+    fi
+  else
+    echo "Skipping ${path}..."
+  fi
+}
+
+for ai in "${AI[@]}"; do
+  for data in "${DATA[@]}"; do
+    echo "${ai}: ${data}"
+    sub::throttle 16
+    run "${ai}" "${data}" &
+  done
+done
