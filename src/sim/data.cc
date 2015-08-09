e6a3adcfbb4890c9a5d37e26de53b553dd4a18ea
sulume <sulume@gmail.com>
2015-08-09 22:35:59 +0900

    fix svg viewbox and small optimization for file size

--- src/sim/data.cc
+++ src/sim/data.cc
@@ -13,13 +13,12 @@ using googleapis::StrAppend;
 using googleapis::StringAppendF;
 
 void Field::print_html(std::ostream& os) const {
-  int w = 1 + width();
-  int h = kSqrt3 * (1 + height());
+  double w = 1 + width();
+  double h = 2 + kSqrt3 * (height() - 1);
   string buf;
   StringAppendF(
     &buf,
-    R"(<svg width=%dpt height=%dpt viewBox="0 0 %d %d" xmlns:xlink="http://www.w3.org/1999/xlink">
-<defs><polygon id="h" points="0,-1 .866,-.5 .866,.5 0,1 -.866,.5 -.866,-.5"/></defs>)", //"
+    R"(<svg width=%.fpt height=%.2fpt viewBox="0 0 %.0f %.2f" fill=#eee><defs><polygon id=h points="0,-1 .866,-.5 .866,.5 0,1 -.866,.5 -.866,-.5"/></defs>)", //"
     w * 10, h * 10, w, h);
   for (int i = 0; i < height(); ++i) {
     for (int j = 0; j < width() / 2; ++j) {
@@ -27,25 +26,25 @@ void Field::print_html(std::ostream& os) const {
       double y = 1 + kSqrt3 * i;
       switch (data[i][j]) {
         case 'x':
-          StringAppendF(&buf, R"(<use x="%.0f" y="%.2f" xlink:href="#h" fill="#e50"/>)", x, y);
+          StringAppendF(&buf, "<use x=%.f y=%.2f xlink:href=#h fill=#e50 />", x, y);
           break;
         case 'o':
-          StringAppendF(&buf, R"(<use x="%.f" y="%.2f" xlink:href="#h" fill="#bf3"/>)", x, y);
+          StringAppendF(&buf, "<use x=%.f y=%.2f xlink:href=#h fill=#bf3 />", x, y);
           break;
         case '@':
-          StringAppendF(&buf, R"(<use x="%.f" y="%.2f" xlink:href="#h" fill="#eee"/>)", x, y);
-          StringAppendF(&buf, R"(<circle cx="%.f" cy="%.2f" r=".2" fill="red"/>)", x, y);
+          StringAppendF(&buf, "<use x=%.f y=%.2f xlink:href=#h />", x, y);
+          StringAppendF(&buf, "<circle cx=%.f cy=%.2f r=.2 fill=red />", x, y);
           break;
         case '&':
-          StringAppendF(&buf, R"(<use x="%.f" y="%.2f" xlink:href="#h" fill="#bf3"/>)", x, y);
-          StringAppendF(&buf, R"(<circle cx="%.f" cy="%.2f" r=".2" fill="red"/>)", x, y);
+          StringAppendF(&buf, "<use x=%.f y=%.2f xlink:href=#h fill=#bf3 />", x, y);
+          StringAppendF(&buf, "<circle cx=%.f cy=%.2f r=.2 fill=red />", x, y);
           break;
         case '$':
-          StringAppendF(&buf, R"(<use x="%.f" y="%.2f" xlink:href="#h" fill="#e50"/>)", x, y);
-          StringAppendF(&buf, R"(<circle cx="%.f" cy="%.2f" r=".2" fill="red"/>)", x, y);
+          StringAppendF(&buf, "<use x=%.f y=%.2f xlink:href=#h fill=#e50 />", x, y);
+          StringAppendF(&buf, "<circle cx=%.f cy=%.2f r=.2 fill=red />", x, y);
           break;
         default:
-          StringAppendF(&buf, R"(<use x="%.f" y="%.2f" xlink:href="#h" fill="#eee"/>)", x, y);
+          StringAppendF(&buf, "<use x=%.f y=%.2f xlink:href=#h />", x, y);
           break;
       }
     }
