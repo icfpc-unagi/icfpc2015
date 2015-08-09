8016d923206b97060e73dfd95e6f4c01ee4014a8
sulume <sulume@gmail.com>
2015-08-09 17:39:12 +0900

    bugfix

--- src/sim/data.cc
+++ src/sim/data.cc
@@ -13,18 +13,18 @@ using googleapis::StrAppend;
 using googleapis::StringAppendF;
 
 void Field::print_html(std::ostream& os) const {
-  int w = 2 + 2 * width();
-  int h = 2 + kSqrt3 * height();
+  int w = 1 + width();
+  int h = 1 + kSqrt3 * height();
   string buf;
   StringAppendF(
     &buf,
     R"(<svg width=%dpt height=%dpt viewBox="0 0 %d %d" xmlns:xlink="http://www.w3.org/1999/xlink">
-<defs><polygon id="h" fill="#eee" points="0,-1 .866,-.5 .866,.5 0,1 -.866,.5 -.866,-.5"/></defs>)", //"
+<defs><polygon id="h" points="0,-1 .866,-.5 .866,.5 0,1 -.866,.5 -.866,-.5"/></defs>)", //"
     w * 10, h * 10, w, h);
   for (int i = 0; i < height(); ++i) {
     for (int j = 0; j < width() / 2; ++j) {
-      double x = 1 + 2 * i + j % 2;
-      double y = 1 + kSqrt3 * j;
+      double x = 1 + 2 * j + i % 2;
+      double y = 1 + kSqrt3 * i;
       switch (data[i][j]) {
         case 'x':
           StringAppendF(&buf, R"(<use x="%.0f" y="%.2f" xlink:href="#h" fill="orange"/>)", x, y);
@@ -47,7 +47,7 @@ void Field::print_html(std::ostream& os) const {
           StringAppendF(&buf, R"(<circle cx="%.f" cy="%.2f" r=".2" fill="red"/>)", x, y);
           break;
         default:
-          StringAppendF(&buf, R"(<use x="%.f" y="%.2f" xlink:href="#h"/>)", x, y);
+          StringAppendF(&buf, R"(<use x="%.f" y="%.2f" xlink:href="#h" fill="#eee"/>)", x, y);
           break;
       }
     }
