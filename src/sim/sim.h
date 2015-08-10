834de4beda77ae43a40888b65eb46ba7cae409f8
sulume <sulume@gmail.com>
2015-08-10 17:57:06 +0900

    aho-corasick power scoring

--- src/sim/sim.h
+++ src/sim/sim.h
@@ -1,9 +1,12 @@
 #ifndef SRC_SIM_SIM_H_
 #define SRC_SIM_SIM_H_
 
+#include <algorithm>
 #include <cstring>
 #include <fstream>
 #include <iostream>
+#include <iterator>
+#include <queue>
 #include <unordered_set>
 #include "base/base.h"
 #include "googleapis/strings/case.h"
@@ -32,31 +35,109 @@ public:
   }
 };
 
+static constexpr int kMap[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,23,3,0,0,0,0,0,2,0,0,0,0,0,0,4,0,5,30,12,6,18,24,0,0,0,0,0,0,0,0,0,0,0,13,7,8,25,9,10,14,15,16,17,31,19,20,21,22,1,26,27,32,33,34,28,35,36,11,29,0,0,0,0,0,0,13,7,8,25,9,10,14,15,16,17,31,19,20,21,22,1,26,27,32,33,34,28,35,36,11,29};
 class Phrases {
-  vector<string> d_;
-public:
+ private:
+  static constexpr int N = 37;
+  struct PMA {
+    PMA* next[N] = {}; // 0: fail
+    vector<int> ok;
+  };
+
+  vector<int> lengths_;
+  const PMA* pma_;
+
+ public:
   Phrases(const string& filename) {
-    if (!filename.empty()) {
-      ifstream ifs(filename);
-      CHECK(ifs.good()) << "Failed to read phrases: " << filename;
-      string s;
-      while (getline(ifs, s)) {
-        googleapis::StripTrailingNewline(&s);
-        googleapis::LowerString(&s);
-        d_.push_back(s);
-      }
+    ifstream ifs(filename);
+    CHECK(ifs.good()) << "Failed to read phrases: " << filename;
+    vector<string> phrases;
+    string s;
+    while (getline(ifs, s)) {
+      googleapis::StripTrailingNewline(&s);
+      googleapis::LowerString(&s);
+      phrases.push_back(s);
+      lengths_.push_back(s.size());
     }
+    pma_ = buildPMA(phrases);
+  }
+  ~Phrases() {
+    releasePMA(pma_);
   }
 
   // Returns power score
-  int spell(StringPiece sp) {
+  int spell(const string& s) const {
     int score = 0;
-    // TODO: make this faster
-    for (const string& phrase : d_) {
-      int reps = CountSubstring(sp, phrase);
-      if (reps > 0) score += 2 * phrase.size() * reps + 300;
+    vector<int> res = match(s);
+    for (int i = 0; i < res.size(); ++i) {
+      if (res[i] > 0) score += 2 * lengths_[i] * res[i] + 300;
     }
     return score;
+  }
+
+ private:
+  static PMA* buildPMA(const vector<string>& patterns) {
+    PMA* root = new PMA;
+    root->next[0] = root;
+    for (int i = 0; i < patterns.size(); ++i) {
+      PMA* t = root;
+      for (int c : patterns[i]) {
+        int m = kMap[c];
+        if (!t->next[m]) t->next[m] = new PMA;
+        t = t->next[m];
+      }
+      t->ok.push_back(i);
+    }
+    queue<PMA*> que;
+    for (int i = 1; i < N; ++i) {
+      if (root->next[i]) {
+        root->next[i]->next[0] = root;
+        que.push(root->next[i]);
+      } else {
+        root->next[i] = root;
+      }
+    }
+    while (!que.empty()) {
+      PMA* t = que.front();
+      que.pop();
+      for (int i = 1; i < N; ++i) {
+        if (t->next[i]) {
+          PMA *r = t->next[0];
+          while (!r->next[i]) r = r->next[0];
+          t->next[i]->next[0] = r->next[i];
+          vector<int> u;
+          set_union(
+              t->next[i]->ok.begin(), t->next[i]->ok.end(),
+              r->next[i]->ok.begin(), r->next[i]->ok.end(),
+              back_inserter(u));
+          t->next[i]->ok.swap(u);
+          que.push(t->next[i]);
+        }
+      }
+    }
+    return root;
+  }
+  vector<int> match(const string& s) const {
+    const PMA* pma = pma_;
+    vector<int> res(lengths_.size());
+    for (int c : s) {
+      int m = kMap[c];
+      while (!pma->next[m]) pma = pma->next[0];
+      pma = pma->next[m];
+      for (int i : pma->ok) res[i]++;
+    }
+   return res;
+  }
+  static void releasePMA(const PMA* root) {
+    unordered_set<const PMA*> v;
+    queue<const PMA*> q;
+    q.push(root);
+    while (!q.empty()) {
+      const PMA* pma = q.front();
+      q.pop();
+      if (v.insert(pma).second) for (int i = 1; i < N; ++i) if (pma->next[i]) q.push(pma->next[i]);
+    }
+    for (const PMA* pma : v) delete pma;
   }
 };
 
@@ -74,7 +155,6 @@ class Sim {
 
  public:
   Field field_;
-  Phrases phrases_ = Phrases(FLAGS_phrases_of_power);
 
   Sim(const Problem& problem, const Solution& solution)
       : problem_(&problem), solution_(&solution),
@@ -166,7 +246,8 @@ class Sim {
       if (logs_ && (!skip_ || i < head_ || s.size() - i <= tail_) && FLAGS_verbose >= 2) *logs_ << "Move Score = " << score << endl;
     }
     if (!FLAGS_phrases_of_power.empty()) {
-      int power_score = phrases_.spell(s);
+      Phrases phrases(FLAGS_phrases_of_power);
+      int power_score = phrases.spell(s);
       if (logs_ && FLAGS_verbose >= 2) *logs_ << "Power Score = " << power_score << endl;
       score += power_score;
     }
