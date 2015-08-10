06a39fe15aeb7030281b332c3d05b41b47a4a773
imos <git@imoz.jp>
2015-08-10 17:58:40 +0900

    Add apt-get to build.sh.

--- build.sh
+++ build.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+sudo apt-get -qqy update
+sudo apt-get -qqy install \
+    build-essential devscripts ubuntu-standard software-properties-common \
+    screen lxc traceroute gdb \
+    vim git subversion mercurial cmake make \
+    dos2unix nkf curl xz-utils \
+    libarchive-dev pkg-config zip g++ zlib1g-dev \
+    php5-cli mono-devel
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 \
+    select true | sudo debconf-set-selections && \
+    sudo add-apt-repository -y ppa:webupd8team/java && \
+    sudo apt-get update -qq && sudo apt-get install -qqy oracle-java8-installer
+
 if which bazel >/dev/null 2>/dev/null; then
   BAZEL='bazel'
 else
